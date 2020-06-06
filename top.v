f80c71495b8518a91449f76bea579eae7fcf1b23
prasadreddy86 <53623592+prasadreddy86@users.noreply.github.com>
2020-06-06 22:09:19 +0530

    Create top.v

---
+++ top.v
@@ -0,0 +1,10 @@
+//Fifo design with 16 bit deapth
+//***************************************************************************//
+
+module fifo(
+             output reg rd_data[n-1:0],
+             output reg ful_emptybar,
+             output reg             
+            );
+
+endmodule
